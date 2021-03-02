63de0debbdfdde327652b72941e411c8c200a6f4
Ivan Hattemer <ivanhattemer@gmail.com>
2021-03-02 14:14:36 +0200

    backend hardcode data

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
 const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+const PORT = 3111
+
+app.get('/', (req, res) => {
+  res.send('API is running ')
+})
+
+app.get('/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/products/:id', (req, res) => {
+  const product = products.find((prod) => prod._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(PORT, () => {
+  console.log('Listening at http://localhost:' + PORT)
+})

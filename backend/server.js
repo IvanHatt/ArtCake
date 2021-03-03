850d8528e47e1978ff885553ec2cb722f747a693
Ivan Hattemer <ivanhattemer@gmail.com>
2021-03-03 12:11:23 +0200

    finished mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
-
+import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
 dotenv.config()
+
+connectDB()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((prod) => prod._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(
     `Listening at http://localhost:${PORT} in ${process.env.NODE_ENV} mode`
+      .yellow.bold
   )
 })

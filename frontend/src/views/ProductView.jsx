7a5d11b0b00b81be1893d211ea785a45028a73c3
Ivan Hattemer <ivanhattemer@gmail.com>
2021-03-11 16:01:15 +0200

    minor chges

--- frontend/src/views/ProductView.jsx
+++ frontend/src/views/ProductView.jsx
@@ -1,16 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import {
-  Row,
-  Col,
-  Image,
-  ListGroup,
-  Card,
-  Button,
-  Form,
-  Meta,
-} from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
@@ -76,7 +67,6 @@ const ProductView = ({ history, match }) => {
         <Message variant='danger'>{error}</Message>
       ) : (
         <>
-          <Meta title={product.name} />
           <Row>
             <Col md={6}>
               <Image src={product.image} alt={product.name} fluid />

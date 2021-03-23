47309f93b4454f61e9ee7a7a32edbd4ede4a51b2
Ivan Hattemer <ivanhattemer@gmail.com>
2021-03-23 17:03:21 +0200

    style
    refactor nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import Image from 'react-bootstrap/Image'
 import { LinkContainer } from 'react-router-bootstrap'
 import { logout } from '../actions/userActions.js'
 import SearchBox from './SearchBox'
@@ -18,18 +19,34 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg='transparent' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>E-commerce</Navbar.Brand>
+            <Navbar.Brand>
+              <Image src='/logoArtCake.png' className='logo'></Image>
+            </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Route render={({ history }) => <SearchBox history={history} />} />
+            {/* <Route render={({ history }) => <SearchBox history={history} />} /> */}
+            <Nav className='m-auto'>
+              <LinkContainer to='/contact'>
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/contact'>
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/contact'>
+                <Nav.Link>Contact</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/contact'>
+                <Nav.Link>Shop</Nav.Link>
+              </LinkContainer>
+            </Nav>
             <Nav className='ml-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
-                  <i className='fas fa-shopping-cart'></i> Cart
+                  <Image src='cart-icon.png' className='cart-icon'></Image>
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
@@ -43,9 +60,7 @@ const Header = () => {
                 </NavDropdown>
               ) : (
                 <LinkContainer to='/login'>
-                  <Nav.Link>
-                    <i className='fas fa-user'></i> Sign In
-                  </Nav.Link>
+                  <Nav.Link className='empty-button'>Sign In</Nav.Link>
                 </LinkContainer>
               )}
               {userInfo && userInfo.isAdmin && (

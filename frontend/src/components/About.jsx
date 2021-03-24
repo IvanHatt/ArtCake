06bc0fa22ea88bb9e2d3a0e9b823191be7ea8d6b
Ivan Hattemer <ivanhattemer@gmail.com>
2021-03-24 15:03:44 +0200

    feat
    add about section

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import about from '../images/about-img.jpg'
 import './styles/about.css'
 
 const About = () => {
-  return <div className='about'>About</div>
+  return (
+    <div className='about'>
+      <Container>
+        <div className='about-container'>
+          <Row>
+            <Col md={7}>
+              <h1 className='text-center'>About Us</h1>
+              <p>
+                Nullam fermentum, lacus a bibendum venenatis, sem eros aliquet
+                ex, at molestie ex orci sed metus. Fusce lobortis, tortor sed
+                rutrum feugiat, sem libero vehicula erat. Nullam fermentum,
+                lacus a bibendum venenatis, sem eros aliquet ex, at molestie ex
+                orci sed metus. Fusce lobortis, tortor sed rutrum feugiat, sem
+                libero vehicula erat. Nullam fermentum, lacus a bibendum
+                venenatis, sem eros aliquet ex, at molestie ex orci sed metus.
+                Fusce lobortis, tortor sed rutrum feugiat, sem libero vehicula
+                erat.
+              </p>
+            </Col>
+            <Col md={5}>
+              <Image src={about} fluid></Image>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </div>
+  )
 }
 
 export default About

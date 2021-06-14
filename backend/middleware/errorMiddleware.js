8b66bce40199a16d48db6f656f7aaf280d57a2e1
Ivan Hattemer <ivanhattemer@gmail.com>
2021-06-14 10:58:26 +0300

    Add add comments

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,8 +1,12 @@
+// custom error handlers
+
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`)
   res.status(404)
   next(error)
 }
+
+
 
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode

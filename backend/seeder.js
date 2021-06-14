ddc0735b333b2bd7d622b7cf753b362119e2faf5
Ivan Hattemer <ivanhattemer@gmail.com>
2021-06-14 10:58:32 +0300

    Add add comments

--- backend/seeder.js
+++ backend/seeder.js
@@ -8,6 +8,8 @@ import Product from './models/productModel.js'
 import Order from './models/orderModel.js'
 import connectDB from './config/db.js'
 
+// only to seed dummy data
+
 dotenv.config()
 
 connectDB()
